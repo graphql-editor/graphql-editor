af6101d8bc8b207685951b78cf76c49d9d756eff
aexol <aexol@aexol.com>
2018-11-30 15:32:55 +0100

    added projects

--- src/cloud/Components/Projects.tsx
+++ src/cloud/Components/Projects.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Subscribe, Provider } from 'unstated';
-import { CloudContainer, Cloud, api } from '../Container';
+import { CloudContainer, Cloud } from '../Container';
 import { Project } from '../ui/Project';
 import { OverlayAdd } from '../ui/OverlayAdd';
 
@@ -14,7 +14,20 @@ export class Projects extends React.Component {
               <OverlayAdd
                 placeholder="Add new project..."
                 onSubmit={(e) => {
-                  api.Mutation.createProject();
+                  cloud
+                    .userApi()
+                    .Mutation.createProject({
+                      name: e
+                    })({
+                      id: true,
+                      name: true,
+                      slug: true
+                    })
+                    .then(({ name, id, slug }) =>
+                      cloud.setState({
+                        projects: [...cloud.state.projects, { name, id, slug }]
+                      })
+                    );
                 }}
               />
               {cloud.state.projects && cloud.state.projects.map((p) => <Project {...p} />)}
--- src/cloud/style/Project.tsx
+++ src/cloud/style/Project.tsx
@@ -1,12 +1,16 @@
 import { style } from 'typestyle';
-
+import * as vars from '../../style/vars';
 export const Project = style({
   width: 240,
   height: 140,
   display: 'flex',
   flexFlow: 'row nowrap',
   alignItems: 'center',
-  justifyContent: 'center'
+  justifyContent: 'center',
+  borderWidth:2,
+  borderColor: vars.overlayText,
+  borderStyle:"solid",
+  borderRadius:10
 });
 export const Name = style({
   textTransform: 'uppercase'
