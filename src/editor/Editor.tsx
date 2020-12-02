3d11844f0f8b3da881c8b3dce62c70285f59e30e
aexol <aexol@aexol.com>
2020-12-02 15:06:20 +0100

    default view changed

--- src/editor/Editor.tsx
+++ src/editor/Editor.tsx
@@ -77,7 +77,7 @@ export const Editor = ({
     libraries: '',
   },
   initialSizeOfSidebar = sizeSidebar,
-  activePane = 'diagram',
+  activePane = 'code-diagram',
   onPaneChange,
   setSchema,
 }: EditorProps) => {
