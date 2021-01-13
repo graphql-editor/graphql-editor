3c92c8778c6026f66d6c7e8e8f8f552941e4f36e
Ania Łysiuk <anna.lysiuk86@gmail.com>
2021-01-13 10:43:09 +0100

    add colors of parent type

--- src/Graf/Node/Type/ActiveType.tsx
+++ src/Graf/Node/Type/ActiveType.tsx
@@ -2,13 +2,26 @@ import React from 'react';
 import { ParserField } from 'graphql-zeus';
 import { GraphQLColors } from '@/editor/theme';
 import { compileTypeOptions } from '@/GraphQL/Compile';
-export const ActiveType: React.FC<Pick<ParserField, 'type'>> = ({ type }) => {
+export const ActiveType: React.FC<Pick<ParserField, 'type'> & { parentTypes?: Record<string, string> }> = ({
+  type,
+  parentTypes,
+}) => {
   let compiledType = compileTypeOptions({ type });
+
+  const getTypeColor = () => {
+    let color = '#fff';
+    if (type.name in GraphQLColors) {
+      color = (GraphQLColors as any)[type.name] as string;
+    }
+    if (parentTypes && type.name in parentTypes) {
+      color = (GraphQLColors as any)[parentTypes[type.name]];
+    }
+
+    return color;
+  };
+
   return (
-    <span
-      className={`TypeColor-${type.name}`}
-      style={{ color: type.name in GraphQLColors ? ((GraphQLColors as any)[type.name] as string) : '#fff' }}
-    >
+    <span className={`TypeColor-${type.name}`} style={{ color: getTypeColor() }}>
       {compiledType}
     </span>
   );
