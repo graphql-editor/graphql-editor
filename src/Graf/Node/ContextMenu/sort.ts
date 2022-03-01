4fbf1d7d886460512db728ad20867229b0cf131d
Jędrzej Dąbrowski <65610189+JedrekDabrowski@users.noreply.github.com>
2022-03-01 21:37:44 +0100

    ⚡️ improve fuzzy search (#313)
    Co-authored-by: JedrekDabrowski <jedrek.dabrowski@aexol.com>

--- src/Graf/Node/ContextMenu/sort.ts
+++ src/Graf/Node/ContextMenu/sort.ts
@@ -1,16 +1,28 @@
 import { ParserField } from 'graphql-js-tree';
 import { match as ftest } from 'fuzzyjs';
 
+const typeFactor = 10;
+
 export const sortNodes = (menuSearchValue: string, fields: ParserField[]) => {
+  const [fieldName, fieldType] = menuSearchValue.split(' ');
   return [...fields].sort((a, b) => {
-    const [aDistance, bDistance] = [
-      ftest(menuSearchValue, a.name),
-      ftest(menuSearchValue, b.name),
+    const [aNameDistance, bNameDistance] = [
+      ftest(fieldName, a.name).score,
+      ftest(fieldName, b.name).score,
     ];
-    return bDistance.score > aDistance.score
-      ? 1
-      : aDistance.score === bDistance.score
+    if (fieldType) {
+      const [aTypeDistance, bTypeDistance] = [
+        ftest(fieldType, a.name).score,
+        ftest(fieldType, b.name).score,
+      ];
+      const aSum = aNameDistance + aTypeDistance * typeFactor;
+      const bSum = bNameDistance + bTypeDistance * typeFactor;
+      return aSum > bSum ? -1 : aSum === bSum ? 0 : 1;
+    }
+    return aNameDistance > bNameDistance
+      ? -1
+      : aNameDistance === bNameDistance
       ? 0
-      : -1;
+      : 1;
   });
 };
