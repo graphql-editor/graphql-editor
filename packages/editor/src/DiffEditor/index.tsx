3ef7aef7b6de028ddf1d9c0ba140100bb5b97bec
JedrekDabrowski <jedrzejdabrowski.jd@gmail.com>
2022-09-05 16:59:08 +0200

    add styles

--- packages/editor/src/DiffEditor/index.tsx
+++ packages/editor/src/DiffEditor/index.tsx
@@ -4,7 +4,7 @@ import React, { useState } from 'react';
 import { Parser, TreeToGraphQL } from 'graphql-js-tree';
 import { useSortState } from '@/state/containers/sort';
 import { fontFamilySans } from '@/vars';
-import { Abc } from '@/editor/icons';
+import { Abc, Arrow } from '@/editor/icons';
 import { useTheme } from '@emotion/react';
 
 interface DiffEditorProps {
@@ -40,17 +40,48 @@ const LineSpacer = styled.div`
   margin: 20px 0;
 `;
 
+const SelectVersionWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Option = styled.div`
+  padding: 15px;
+  color: ${({ theme }) => theme.text};
+  font-size: 14px;
+  font-family: ${fontFamilySans};
+  background-color: ${({ theme }) => theme.background.mainMiddle};
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  &:hover {
+    background-color: ${({ theme }) => theme.background.mainMiddle};
+  }
+`;
+
+const OptionsWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Placeholder = styled.p`
+  margin-right: 16px;
+`;
+
 export const DiffEditor = ({ schemas }: DiffEditorProps) => {
   const { sortAlphabetically } = useSortState();
-  const { inactive, active } = useTheme();
+  const { inactive, active, text } = useTheme();
 
   const [isSortActive, setIsSortActive] = useState(false);
   const [choosenLeftVersion, setChoosenLeftVersion] = useState(
-    Object.keys(schemas)[Object.keys(schemas).length - 1],
+    Object.keys(schemas)[Object.keys(schemas).length - 2],
   );
   const [choosenRightVersion, setChoosenRightVersion] = useState(
-    Object.keys(schemas)[Object.keys(schemas).length],
+    Object.keys(schemas)[Object.keys(schemas).length - 1],
   );
+  const [isLeftOpen, setIsLeftOpen] = useState(false);
+  const [isRightOpen, setIsRightOpen] = useState(false);
 
   const sortSchema = (schema: string) => {
     const tree = Parser.parse(schema);
@@ -70,22 +101,54 @@ export const DiffEditor = ({ schemas }: DiffEditorProps) => {
         </div>
       </TopBar>
       <TopBar>
-        <div>
-          <p>SELECT VERSION:</p>
-          <select onChange={(e) => setChoosenLeftVersion(e.target.value)}>
-            {Object.keys(schemas).map((s) => (
-              <option>{s}</option>
-            ))}
-          </select>
-        </div>
-        <div>
-          <p>SELECT VERSION:</p>
-          <select onChange={(e) => setChoosenRightVersion(e.target.value)}>
-            {Object.keys(schemas).map((s) => (
-              <option>{s}</option>
-            ))}
-          </select>
-        </div>
+        <SelectVersionWrapper>
+          <Heading>SELECT VERSION</Heading>
+          <Option>
+            <Placeholder onClick={() => setIsLeftOpen(true)}>
+              {choosenLeftVersion || 'Select version'}
+            </Placeholder>
+            <Arrow size={12} fill={text} />
+          </Option>
+          {isLeftOpen && (
+            <OptionsWrapper>
+              {Object.keys(schemas).map((s, i) => (
+                <Option
+                  onClick={() => {
+                    setIsLeftOpen(false);
+                    setChoosenLeftVersion(s);
+                  }}
+                  key={i}
+                >
+                  {s}
+                </Option>
+              ))}
+            </OptionsWrapper>
+          )}
+        </SelectVersionWrapper>
+        <SelectVersionWrapper>
+          <Heading>SELECT VERSION</Heading>
+          <Option>
+            <Placeholder onClick={() => setIsRightOpen(true)}>
+              {choosenRightVersion || 'Select version'}
+            </Placeholder>
+            <Arrow size={12} fill={text} />
+          </Option>
+          {isRightOpen && (
+            <OptionsWrapper>
+              {Object.keys(schemas).map((s, i) => (
+                <Option
+                  onClick={() => {
+                    setIsRightOpen(false);
+                    setChoosenRightVersion(s);
+                  }}
+                  key={i}
+                >
+                  {s}
+                </Option>
+              ))}
+            </OptionsWrapper>
+          )}
+        </SelectVersionWrapper>
       </TopBar>
       <LineSpacer />
       <DiffEditorPane
