fd6925a8924409addff57736d1f73b45e313bbd1
aexol <aexol@aexol.com>
2022-09-08 15:48:42 +0200

    :bug: Fixed default value convertion

--- packages/editor/src/GraphQL/Convert/index.tsx
+++ packages/editor/src/GraphQL/Convert/index.tsx
@@ -1,35 +1,6 @@
 import { isScalarArgument } from '@/GraphQL/Resolve';
-import {
-  compileType,
-  getTypeName,
-  Options,
-  Parser,
-  ParserField,
-  Value,
-} from 'graphql-js-tree';
+import { Options, ParserField, Value } from 'graphql-js-tree';
 import { TemplateUtils } from 'graphql-js-tree/lib/TreeToGraphQL/templates/TemplateUtils';
-export const ConvertStringToObject = (
-  value: string,
-  typeName: string,
-  fieldType: string,
-) => {
-  const computeString = `
-    scalar ${typeName}
-    input Translate{
-        field: ${fieldType} = ${value}
-    }
-`;
-  const TranslatedString = Parser.parse(computeString);
-
-  const translationNode = TranslatedString.nodes.find(
-    (n) => n.name === 'Translate',
-  );
-  if (!translationNode || !translationNode.args) {
-    return;
-  }
-  const fieldNode = translationNode.args[0];
-  return fieldNode.args;
-};
 
 export const ConvertValueToEditableString = (f: ParserField) => {
   const TranslatedString = TemplateUtils.resolverForConnection(f);
@@ -82,9 +53,21 @@ export const placeStringInNode = ({ node, v }: PlaceFunctionArgs) => {
     };
     return [n];
   }
-  return ConvertStringToObject(
-    v,
-    getTypeName(node.type.fieldType),
-    compileType(node.type.fieldType),
-  );
+  return [
+    {
+      data: {
+        type: Value.IntValue,
+      },
+      type: {
+        fieldType: {
+          name: Value.IntValue,
+          type: Options.name,
+        },
+      },
+      name: value,
+      directives: [],
+      args: [],
+      interfaces: [],
+    } as ParserField,
+  ];
 };
