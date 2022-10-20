57ff4e7564e981bfa01b38bfd918e5d8a75ddf24
Damian Aexol <90246296+DamianAexol@users.noreply.github.com>
2022-10-20 16:46:21 +0200

    fixed node deselect on routing, fixed onRouteChange not firing when last state change was from routeState prop, added external router usecase to googleState sandbox (#500)

---
+++ packages/sandbox/helpers/FakeRouter.tsx
@@ -0,0 +1,71 @@
+import { useState } from 'react';
+import * as qs from 'query-string';
+
+export interface Routes {
+  code?: string;
+  pane?: string;
+  n?: string;
+  a?: string;
+}
+
+export const useRouter = () => {
+  const resolvePath = () => {
+    const { code, pane, n } = qs.parse(window.location.search) as Routes;
+    return {
+      code,
+      pane,
+      n,
+    };
+  };
+
+  const [path, setPath] = useState<Routes>(resolvePath());
+
+  const route = ({
+    path = window.location.pathname,
+    location = window.location.search,
+    fullPath,
+  }: {
+    path?: string;
+    location?: string;
+    fullPath?: string;
+  }) => {
+    const finalPath = `/${fullPath ?? [path, location].join('')}`;
+    window.history.pushState({}, '', finalPath);
+  };
+  const composePath = (r: Routes, p: Routes, skipCleanup?: true) => {
+    let path = {
+      ...p,
+      ...r,
+    };
+
+    let fullPath = '';
+
+    const params: string[] = [];
+    const iterableParams: Array<keyof Routes> = ['code', 'pane', 'n', 'a'];
+    iterableParams
+      .filter((ip) => path[ip])
+      .forEach((ip) => {
+        params.push(`${ip}=${path[ip]}`);
+      });
+    if (params.length) {
+      fullPath += `?${params.join('&')}`;
+    }
+    return { fullPath, path };
+  };
+
+  const changeRoute = (r: Routes) => {
+    setPath((p) => {
+      const { fullPath, path } = composePath(r, p);
+      route({ fullPath });
+      return path;
+    });
+  };
+
+  return {
+    route,
+    changeRoute,
+    path,
+    setPath,
+    resolvePath,
+  };
+};
