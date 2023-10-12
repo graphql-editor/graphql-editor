dbac57d6e5efe4bdc05d77f1e8bcc0ba361a672d
aexol <aexol@aexol.com>
2023-10-12 23:09:59 +0200

    :bug: Fixed fromLibrary

--- packages/editor/src/shared/components/ContextMenu/NodeAddFieldMenu/index.tsx
+++ packages/editor/src/shared/components/ContextMenu/NodeAddFieldMenu/index.tsx
@@ -62,6 +62,7 @@ export const NodeAddFieldMenu = React.forwardRef<
         value: undefined,
         interfaces: [],
         directives: [],
+        fromLibrary: false,
         description: "",
         type: {
           fieldType: {
